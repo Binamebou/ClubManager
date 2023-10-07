40e0ed5ea99d6485cb5101b5f05ea19c2ff62f1e
Benoit Oury <benoit.oury@gmail.com>
2023-10-07 13:01:40 +0200

    Remove spaces from user login at logon

--- www/utils.php
+++ www/utils.php
@@ -4,12 +4,13 @@ class utils
 {
 
     public function normalize($entry) {
-        $entry = preg_replace('/[éèëê]/u',"e",$entry);
-        $entry = preg_replace('/[àâä]/u',"a",$entry);
-        $entry = preg_replace('/[îï]/u',"i",$entry);
-        $entry = preg_replace('/[ôö]/u',"o",$entry);
-        $entry = preg_replace('/[ûü]/u',"u",$entry);
-        $entry = preg_replace('/[^A-Za-z0-9\.]/',"",$entry);
+        $entry = preg_replace('/[éèëê]/ui',"e",$entry);
+        $entry = preg_replace('/[àâä]/ui',"a",$entry);
+        $entry = preg_replace('/[îï]/ui',"i",$entry);
+        $entry = preg_replace('/[ôö]/ui',"o",$entry);
+        $entry = preg_replace('/[ûü]/ui',"u",$entry);
+        $entry = preg_replace('/[ç]/ui',"ç",$entry);
+        $entry = preg_replace('/[^A-Za-z0-9\.]/ui',"",$entry);
         return $entry;
     }
 
